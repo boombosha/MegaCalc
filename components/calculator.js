eba320118ae3e1c3f2cd5611e689ddff567512b0
boombosha <boombosha@gmail.com>
2024-04-12 14:56:42 +0300

    styling

--- components/calculator.js
+++ components/calculator.js
@@ -2,6 +2,46 @@ app.component('mega-calc', {
     template:
     /*html*/
     `<div class="calc-container">
+        <!--<p> <span class="checkbox-name">Хочу конвертировать бонусы в рубли:</span>  
+            <label class="checkbox style-e">
+                <input type="checkbox" v-model="toggle" checked="checked"/>
+                <div class="checkbox__checkmark"></div>
+                <div class="checkbox__body"></div>
+            </label>
+        </p>
+        <p v-if="toggle"> -->
+
+            <div class="radio-container">
+                <p> Без пакета услуг: <label><input type="radio" name="sber" v-model="selectedOption" :value="'withoutPackage'"></label> </p>
+                <p> СберПрайм+: <label><input type="radio" name="sber" :value="'sberPrime'" v-model="selectedOption"></label> </p>          
+                <p> СберПремьер: <label><input type="radio" name="sber" :value="'sberPremier'" v-model="selectedOption"></label> </p>
+                <p> СберПервый: <label><input type="radio" name="sber" :value="'sberFirst'" v-model="selectedOption"></label></p>
+                <p> Sber Private Banking: <label><input type="radio" name="sber" :value="'sberPrivate'" v-model="selectedOption"></label></p>
+                <p> <input type="number" v-model="bonusGive" class="bonusGive" placeholder="бонусы"><img src="./assets/pic/sberspasibo.png" alt="сберспасибо" width="50" align="top"> = <span class="moneyReturn"> {{ moneyReturn }} </span> ₽</p>
+            </div>    
+              
+            
+        </p>       
+        <p> Сумма заказа: <input type="number" v-model.number="orderSumm" placeholder="Сумма заказа"> руб.</p>
+        <p> Обещают бонусов: <input type="number" v-model.number="bonusPromise" placeholder="Обещают бонусов"> %</p>
+        <p> Промокод: <input type="number" v-model.number="promocode" placeholder="Промокод"> руб.</p>
+        <p> Оплата бонусами: <input type="number" v-model.number="bonusPay" placeholder="Оплата бонусами"> <img src="./assets/pic/sberspasibo.png" alt="сберспасибо" width="50" align="top"></p>
+        <div class="alert" v-show="(orderSumm-promocode)<bonusPay">
+            <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+            <strong> Сумма оплаты бонусами не может превышать сумму заказа </strong>
+        </div>    
+        <p> Вернут бонусов: <span class="bonusBack"> {{ bonusBack }} </span> <img src="./assets/pic/sberspasibo.png" alt="сберспасибо" width="50" align="top"></p>
+        <p> Спишут с карты: <span class="realPayment"> {{ realPayment }} </span> руб.</p>
+        
+        
+        <p> Цена товара: <span class="realPrice"> {{ priceNoCashbackConv }} </span>
+            <br><span class="realPriceExplanation">(если не конвертируем кэшбек) </span>  
+        </p>
+        
+        <p> Цена товара: <span class="realPrice"> {{ priceCashbackConv }} </span>
+            <br><span class="realPriceExplanation">(если конвертируем кэшбек) </span>  
+        </p>
+
         <p> <span class="checkbox-name">Хочу конвертировать бонусы в рубли:</span>  
             <label class="checkbox style-e">
                 <input type="checkbox" v-model="toggle" checked="checked"/>
@@ -9,31 +49,11 @@ app.component('mega-calc', {
                 <div class="checkbox__body"></div>
             </label>
         </p>
-        <p v-if="toggle">
-            <div class="radio-container">
-                <p> Без пакета услуг: <label><input type="radio" name="sber" v-model="selectedOption" :value="'withoutPackage'"></label> </p>
-                <p> СберПрайм+: <label><input type="radio" name="sber" :value="'sberPrime'" v-model="selectedOption"></label> </p>          
-                <p> СберПремьер: <label><input type="radio" name="sber" :value="'sberPremier'" v-model="selectedOption"></label> </p>
-                <p> СберПервый: <label><input type="radio" name="sber" :value="'sberFirst'" v-model="selectedOption"></label></p>
-                <p> Sber Private Banking: <label><input type="radio" name="sber" :value="'sberPrivate'" v-model="selectedOption"></label></p>
-            </div>    
-            <p> Отдаю бонусов: <input type="number" v-model="bonusGive"> руб.</p>            
-            <p> Получаю: <span class="moneyReturn"> {{ moneyReturn }} </span> руб.</p>
-            
-        </p>       
-        <p> Сумма заказа: <input type="number" v-model.number="orderSumm" placeholder="Сумма заказа"> руб.</p>
-        <p> Обещают бонусов: <input type="number" v-model.number="bonusPromise" placeholder="Обещают бонусов"> %</p>
-        <p> Промокод: <input type="number" v-model.number="promocode" placeholder="Промокод"> руб.</p>
-        <p> Оплата бонусами: <input type="number" v-model.number="bonusPay" placeholder="Оплата бонусами"> руб.</p>
-        <div class="alert" v-show="(orderSumm-promocode)<bonusPay">
-            <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
-            <strong> Сумма оплаты бонусами не может превышать сумму заказа </strong>
-        </div>    
-        <p> Вернут бонусов: <span class="bonusBack"> {{ bonusBack }} </span> руб.</p>
-        <p> Спишут с карты: <span class="realPayment"> {{ realPayment }} </span> руб.</p>
-        <p> Истинная цена товара: <span class="realPrice"> {{ realPrice }} </span>
-            <br><span class="realPriceExplanation">(с учётом конвертации бонусов) </span>  
-        </p>
+        <div v-if="toggle">
+            Бонусы: <input type="number" v-model.number="bonusToConvert" placeholder="Конвертируемые бонусы"> <img src="./assets/pic/sberspasibo.png" alt="сберспасибо" width="50" align="top">
+            <p> Цена товара: <span class="realPrice"> {{ priceBonusConv }} </span></p>
+            <span class="realPriceExplanation"> (если конвертируем ранее накопленные бонусы) </span>  
+        </div>
         
     </div>`,
     data() {
@@ -54,7 +74,8 @@ app.component('mega-calc', {
     },
     computed: {
         bonusBack() {
-            return (this.orderSumm - this.promocode - this.bonusPay) * this.bonusPromise / 100
+            let bonusBack = ((this.orderSumm - this.promocode - this.bonusPay) * this.bonusPromise / 100);
+            return bonusBack
         },
         realPayment() {
             if ( (this.orderSumm - this.promocode - this.bonusPay) > 0 ) {
@@ -77,9 +98,11 @@ app.component('mega-calc', {
             }
             if (this.selectedOption === 'sberFirst' || this.selectedOption === 'sberPrivate') {
                 return this.bonusGive * 0.8
-            }
-            
-            
+            }           
+        },
+        priceNoCashbackConv() {
+            // return realPayment() + ( this.bonusGive - moneyReturn () )
+            return (this.orderSumm - this.promocode + this.bonusPay) 
         }
        
 
